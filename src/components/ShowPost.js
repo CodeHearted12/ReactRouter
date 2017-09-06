c4ce409b4c8753d0a86e313c2e53acab45fac4d0
Myron Anthony <codehearted12@gmail.com>
2017-09-06 01:14:30 -0400

    progress

---
+++ src/components/ShowPost.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+export default class ShowPost extends Component {
+
+constructor (props) {
+  super(props)
+
+  this.state = {
+    blog: {}
+  }
+}
+componentDidMount () {
+    const { id } = this.props.match.params
+    const URL = `https://tiny-lasagna-server.herokuapp.com/collections/blogger/${id}`
+    fetch(URL).then(response => {
+      return response.json()
+    }).then(data => {
+        console.log(data)
+        this.setState({blog: data})
+      })
+  }
+  render () {
+    let BigBlog = this.state.blog
+    return (
+      <div className='show-post'>
+        <div className="card">
+          <h3 className="card-header">{BigBlog.title}</h3>
+          <div className="card-block">
+            <h4 className="card-title">By: {BigBlog.name}</h4>
+            <p className="card-text">{BigBlog.blog}</p>
+            <Link to='/show' className="btn btn-primary">Back to all blog posts</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import App from './components/App'
+import BaseLayout from './components/Layout'
+import CreatePost from './components/CreatePost'
+import PostList from './components/PostList'
+import ShowPost from './components/ShowPost'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
